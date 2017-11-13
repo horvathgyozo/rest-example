db3d58bf1550c002893d1d0e545c240da96bad1a
Győző Horváth <gyozo.horvath@inf.elte.hu>
2017-11-13 15:22:04 +0100

    feathers generate service recipes

---
+++ rest-feathers-cli/src/models/recipes.model.js
@@ -0,0 +1,26 @@
+// See http://docs.sequelizejs.com/en/latest/docs/models-definition/
+// for more of what you can do here.
+const Sequelize = require('sequelize');
+const DataTypes = Sequelize.DataTypes;
+
+module.exports = function (app) {
+  const sequelizeClient = app.get('sequelizeClient');
+  const recipes = sequelizeClient.define('recipes', {
+    title: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    ingredients: DataTypes.TEXT
+  }, {
+    hooks: {
+      beforeCount(options) {
+        options.raw = true;
+      }
+    }
+  });
+
+  recipes.associate = function (models) { // eslint-disable-line no-unused-vars
+    // Define associations here
+    // See http://docs.sequelizejs.com/en/latest/docs/associations/
+  };
+
+  return recipes;
+};
