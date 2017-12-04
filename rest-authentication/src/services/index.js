ecaed13a73e6c6e31c9f3e56e2cc89860e54d7a4
Győző Horváth <gyozo.horvath@inf.elte.hu>
2017-12-04 23:21:12 +0100

    adding recipes service to rest-authentication

--- rest-authentication/src/services/index.js
+++ rest-authentication/src/services/index.js
@@ -1,5 +1,7 @@
 const users = require('./users/users.service.js');
+const recipes = require('./recipes/recipes.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(users);
+  app.configure(recipes);
 };
